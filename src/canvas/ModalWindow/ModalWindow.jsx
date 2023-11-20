00c79cdf423ceee8f3ed1a6940301579048cb925
macOs <max.kriv3228@gmail.com>
2023-11-20 14:15:52 +0300

    start create modal

---
+++ src/canvas/ModalWindow/ModalWindow.jsx
@@ -0,0 +1,12 @@
+import '../../assets/css/canvas/ModalWindow/ModalWindow.css';
+
+const Landing = () => {
+
+	return (
+		<div className='modal-window'>
+            
+		</div>
+	);
+};
+
+export default Landing;
